211f7e7a8eb443a15dbd38674a5214f982be81e0
Jared Steiner <steiner.jared1996@gmail.com>
2021-07-07 18:31:50 +1000

    Add Health Check Integration Test

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/health", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
+    run()?.await
 }
---
+++ tests/health.rs
@@ -0,0 +1,23 @@
+#[actix_rt::test]
+async fn health_check_returns_ok() {
+    // Arrange
+    spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get("http://127.0.0.1:8000/health")
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+
+    tokio::spawn(server);
+}
